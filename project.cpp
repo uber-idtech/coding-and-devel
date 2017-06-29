211fe2e6944aca4066678360840212951565a088
uber-idtech <maryvo.mvo@gmail.com>
2017-06-29 11:54:29 -0400

    bloop

---
+++ project.cpp
@@ -0,0 +1,176 @@
+#include "project.h"
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Monday, November 11." << endl;
+	cout << "Welcome, Troye, to Online Chat Room." << endl;
+	cout << "Bailey has logged in.\n" << endl;
+	string UserInput; 
+	getch();
+
+	cout << "Bailey: Hey Troye.\n" << endl;
+
+	getch();
+	cout << "Troye: What do you want Bailey?\n" << endl;
+
+	getch();
+	cout << "Bailey: Wow, chill, just saying Hi." << endl;
+	cout << "I don't understand why you are still mad at me.\n" << endl;
+
+	getch();
+	cout << "Troye: You just annoy me.\n" << endl;
+
+	getch();
+	cout << "Bailey: It is my role in life haha.\n" << endl;
+
+	getch();
+	cout << "Troye: Really Bailey, why are you here?\n" << endl;
+
+	getch();
+	cout << "Bailey: Just talking to my buddy Troye haha.\n" << endl;
+
+	getch();
+	cout << "Troye: ...You need something again...for what?\n" << endl;
+	
+	getch();
+	cout << "Bailey: I never said anything of the sort." << endl;
+	cout << "...But I can make you a deal.\n" << endl;
+
+	getch();
+	cout << "Troye: There is nothing you can offer me.\n" << endl;
+
+	getch();
+	cout << "Bailey: Well how about I leave you alone if you do me a favor." << endl;
+	cout << "A large box came to the post office today, I need you to take your truck to pick it up.\n" << endl;
+
+	getch();
+	cout << "Troye: What's in the box?\n" << endl;
+
+	getch();
+	cout << "Bailey: That is classified information. I expect to see it at my house tomorrow." << endl;
+
+	getch();
+	cout << "Bailey has logged out.\n" << endl;
+
+	getch();
+	cout << "Tuesday, November 12." << endl;
+	cout << "Welcome back, Troye, to Chat Room." << endl;
+	cout << "Bailey has logged in.\n" << endl;
+
+	getch();
+	cout << "Troye: Bailey what is in this box?\n" << endl;
+
+	getch();
+	cout << "Bailey: Honestly don't remember. haha.\n" << endl;
+
+	getch();
+	cout << "Troye: The box is giant, but there is nothing in it...\n" << endl;
+
+	getch();
+	cout << "Bailey: Well just don't open it.\n" << endl; 
+
+	getch();
+	cout << "Troye: Relax I didn't do anything. I shouldn't have even agreed to this.\n" << endl;
+
+	getch();
+	cout << "Bailey: Whatever. I'm not at the house ,but just walk in and leave it in the kitchen. \n" << endl;
+	
+	getch();
+	cout << "Troye: I just pulled in." << endl;
+
+	getch();
+	cout << "Bailey your front door is locked.\n" << endl;
+
+	getch();
+	cout << "Bailey: Try the other doors.\n" << endl;
+
+	getch();
+	cout << "Troye: Those are locked too. Is someone home?\n" << endl;
+
+	getch();
+	cout << "Bailey: No.\n" << endl; 
+
+	getch();
+	cout << "Troye: I hear screams. There is a girl, she is pounding on the door.\n" << endl;
+
+	getch();
+	cout << "Bailey: Don't let her out.\n" << endl;
+
+	getch();
+	char ReaderInput;
+		cout << "A- Troye: BAILEY! SHE NEEDS HELP!\n" << endl;
+	cout << "B- Troye: Ummm... Ok then.\n" << endl;
+	cin >> ReaderInput;
+	if (ReaderInput = 'A')
+	{
+		cout << "Bailey: You just broke my window!\n" << endl;
+	}
+	else 
+	{
+		cout << "Bailey: Why are you talking to her?\n" << endl;
+	}
+	
+	cout << "Troye: I had to help the girl. Wait. I thought you weren't home." << endl;
+
+	getch();
+	cout << "Bailey logged out.\n" << endl;
+
+	getch();
+	cout << "Chat Room destroyed, log out?\n" << endl;
+	cout << "OK" << endl;
+
+	getch();
+	cout << "Saturday, November 16." << endl;
+	cout << "New Chat Room created." << endl;
+	cout << "Sharkeisha logged in.\n" << endl;
+
+	getch();
+	cout << "Sharkeisha: Hey Troye?\n" << endl;
+
+	getch();
+	cout << "Troye: Sup Sharky?\n" << endl;
+
+	getch();
+	cout << "Sharkeisha: Have you seen Bailey at school? I keep calling him but he doesn't pick up." << endl;
+	cout << "His home line gives you a busy signal.\n" << endl;
+
+	getch();
+	cout << "Troye: No, I haven't seen him.\n" << endl;
+
+	getch();
+	cout << "Sharkeisha: Should I be worried?\n" << endl;
+
+	getch();
+	cout << "Troye: Nah. If you want, I can go to his house after school just to check on him.\n" << endl;
+
+	getch();
+	cout << "Sharkeisha: That would be great, Thanks Troye. See you in Homeroom!\n" << endl;
+
+	getch();
+	cout << "Sharkeisha logged out." << endl;
+
+	getch();
+	cout << "Bailey logged in.\n" << endl;
+
+	getch();
+	cout << "Bailey: So are you actually going to show up or just lie to your friend?\n" << endl;
+
+	getch();
+	cout << "Troye: BAILEY?" << endl;
+	cout << "You have everyone nervous. Why are you not at school?\n" << endl;
+
+	getch();
+	cout << "Bailey: Answer my question first. Maybe I will answer yours.\n" << endl;
+
+	getch();
+	cout << "Troye: Well I'm not going to lie if she is worried.\n" << endl;
+
+	getch();
+	cout << "Bailey: Have fun after school Troye hehe. Sharkisha won't be screaming this time.\n" << endl;
+
+	getch();
+	cout << "Bailey has logged out." << endl;
+}
